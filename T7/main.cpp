6a0766a4c6446b3438923a1ece8b81b4c2ac0fe7
Mauricio Pedro Vieira <mauriciopsgv@gmail.com>
2017-06-19 11:41:37 -0300

    unionfind working

--- T7/main.cpp
+++ T7/main.cpp
@@ -1,10 +1,32 @@
-#include <iostream>
+ #include <iostream>
 
 #include "unionfind.h"
 
+#define UFSIZE 5
+
+using namespace std;
 
 int main(void)
 {
+	UnionFind * uf = new UnionFind(5);
+
+	for (int i = 0; i < UFSIZE; i++)
+	{
+		cout << "Representante de " << i << " eh " << uf->find(i) << endl;
+	}
+
+	uf->makeUnion(3, 4);
+	uf->makeUnion(2, 3);
+	uf->makeUnion(1, 5);
+	uf->makeUnion(0, 1);
+	uf->makeUnion(1, 1);
+	uf->makeUnion(-1, 5);
+	uf->makeUnion(4, 6);
+
+	for (int i = 0; i < UFSIZE; i++)
+	{
+		cout << "Representante de " << i << " eh " << uf->find(i) << endl;
+	}
 
 	return 0;
 }
--- T7/unionfind.cpp
+++ T7/unionfind.cpp
@@ -16,12 +16,33 @@ UnionFind::~UnionFind()
 
 int UnionFind::find(int u)
 {
-    return (parent[u] == -1) ? u : find(u);
+    return (parent[u] == -1) ? u : find(parent[u]);
 }
 
 
 void UnionFind::makeUnion(int u, int v)
 {
+	if (u == v ||
+		u < 0  || u >= (int) parent.size() || 
+		v < 0  || v >= (int) parent.size() )
+		return;
+
+	int aux;
+	int repU = find(u);
+	int repV = find(v);
+	if (repU == repV)
+		return;
+
+	size[repU] += size[repV];
+	size[repV] = 0;
+
+	while (parent[v] != -1)
+	{
+		aux = v;
+		v = parent[v];
+		parent[aux] = repU;
+	}
+	parent[v] = repU;
 }
 
 
